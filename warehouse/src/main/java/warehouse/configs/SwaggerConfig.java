68fd9a4ef3db3cad2f5879db1a31647d56e5fabe
andrey1510 <andrey-1510@outlook.com>
2023-02-24 19:29:05 +0300

    Warehouse service - docker and swagger updated, liquibase WIP.

--- warehouse/src/main/java/warehouse/configs/SwaggerConfig.java
+++ warehouse/src/main/java/warehouse/configs/SwaggerConfig.java
@@ -9,18 +9,18 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 
 //@Configuration
-public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
-        return new OpenAPI().info(new Info().title("test API")
-                .version(appVersion)
-                .description("test Swagger.")
-                .termsOfService("http://swagger.io/terms/")
-                .contact(new Contact()
-                        .name("test")
-                        .email("test@test.ru")
-                        .url("test"))
-                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
-    }
-}
+//public class SwaggerConfig {
+//
+//    @Bean
+//    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+//        return new OpenAPI().info(new Info().title("test API")
+//                .version(appVersion)
+//                .description("test Swagger.")
+//                .termsOfService("http://swagger.io/terms/")
+//                .contact(new Contact()
+//                        .name("test")
+//                        .email("test@test.ru")
+//                        .url("test"))
+//                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+//    }
+//}
